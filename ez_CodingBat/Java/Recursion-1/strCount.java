6c628b7f6cbe2fc988a5054c395bc82838dd3bb9
macaoleaf <yezisheng1997@gmail.com>
2018-01-14 23:06:58 -0500

    recur-1 finished

---
+++ ez_CodingBat/Java/Recursion-1/strCount.java
@@ -0,0 +1,16 @@
+/* Given a string and a non-empty substring sub, 
+ * compute recursively the number of times that sub appears in the string, 
+ * without the sub strings overlapping.
+
+
+strCount("catcowcat", "cat") ¡ú 2
+strCount("catcowcat", "cow") ¡ú 1
+strCount("catcowcat", "dog") ¡ú 0*/
+
+public int strCount(String str, String sub) {
+  if(str.length() < sub.length()) return 0;
+  if(str.substring(0, sub.length()).equals(sub)){
+    return 1 + strCount(str.substring(sub.length()), sub);
+  }
+  return strCount(str.substring(1), sub);
+}
