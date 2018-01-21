93ad338d7c68af2ba2d7126007bfe8f24448aebb
macaoleaf <yezisheng1997@gmail.com>
2018-01-21 15:04:55 -0500

    476 added

---
+++ nm_LeetCode/Algorithms/ez/476 Number Complement.java
@@ -0,0 +1,37 @@
+/* Given a positive integer, output its complement number. 
+ * The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer¡¯s binary representation.
+
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. 
+So you need to output 2.
+
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), 
+and its complement is 0. So you need to output 0.*/
+
+class Solution {
+  public int findComplement(int num) {
+    if(num < 2) return 0;
+    int i = 0;
+    int result = 0;
+    int temp = num;
+    while(temp != 0){
+      i++;
+      temp /= 2;
+    }
+    int sum = 1;
+    for(int n = 0; n < i; n++){
+      sum *= 2;
+    }
+    result = sum - 1 - num;
+    return result;
+  }
+}
