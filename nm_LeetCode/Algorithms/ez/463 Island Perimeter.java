1878384ef3830ae5bb5db81c7ceea03b256aed34
macaoleaf <yezisheng1997@gmail.com>
2018-01-26 01:10:11 -0500

    463 added

---
+++ nm_LeetCode/Algorithms/ez/463 Island Perimeter.java
@@ -0,0 +1,37 @@
+/* You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. 
+ * Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
+ * and there is exactly one island (i.e., one or more connected land cells). 
+ * The island doesn't have "lakes" (water inside that isn't connected to the water around the island). 
+ * One cell is a square with side length 1. 
+ * The grid is rectangular, width and height don't exceed 100. 
+ * Determine the perimeter of the island.
+
+Example:
+
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Answer: 16*/
+
+class Solution {
+  public int islandPerimeter(int[][] grid) {
+    int peri = 0;
+    for(int i = 0; i < grid.length; i++){
+      for(int j = 0; j < grid[i].length; j++){
+        if(grid[i][j] == 1){
+          if(i == 0) peri++;         
+          if(i == (grid.length - 1)) peri++;
+          if(j == 0) peri++;
+          if(j == (grid[i].length - 1)) peri++;
+          if(i > 0 && grid[i - 1][j] == 0) peri++;
+          if(j > 0 && grid[i][j - 1] == 0) peri++;
+          if(i < (grid.length - 1) && grid[i + 1][j] == 0) peri++;
+          if(j < (grid[i].length - 1) && grid[i][j + 1] == 0) peri++;
+        }
+      }
+    }
+    return peri;
+  }
+}
